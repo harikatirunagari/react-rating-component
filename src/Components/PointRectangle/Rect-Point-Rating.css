602d9706cd26f8d57607b850aa5705b8f6601c0b
Harika Tirunagari <harikasatagopam@gmail.com>
2019-05-02 14:41:58 +0800

    Create Rect-Point-Rating.css

---
+++ src/Components/PointRectangle/Rect-Point-Rating.css
@@ -0,0 +1,12 @@
+.rectangle{
+    height:50px;
+    width:50px;
+    border:1px solid #ccc;
+    margin:5px;
+    border-radius:50%;
+    display:inline-block;   
+}
+
+.rectangle:hover{
+    cursor: pointer;
+}
